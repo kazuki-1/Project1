ab88f9916cd808fff939fcf90951a4a8a12bae22
mysamrung <mysamrung@gmail.com>
2021-01-15 22:50:51 +0900

    Update Player.cpp

--- SourceCode/Player.cpp
+++ SourceCode/Player.cpp
@@ -10,6 +10,7 @@ extern std::vector<Fan>dist;
 Player player;
 
 Player_Animation animation;
+Fan* pushedFan;
 
 using namespace GameLib::input;
 void Player::Initialize(GameLib::Sprite* sp, VECTOR2 p, VECTOR2 s, VECTOR2 tp, VECTOR2 ts)
@@ -26,13 +27,19 @@ bool FanCollision(VECTOR2 player_pos, VECTOR2 fan_pos)
     return temp == fan_pos;
 }
 
+int sign(int val) {
+    if (val < 0) return -1;
+    else if (val > 0) return 1;
+    return 0;
+}
+
 #define XSIZE 54
 #define YSIZE 108
 void Player::Update()
 {
     speed.x = {};
     const VECTOR2 tpos{ pos };
-    if (STATE(0) & PAD_LEFT) {
+    if (STATE(0) & PAD_LEFT) { 
         player.tPos = animation.GetAnimation_Offset(Player_Animation::STATE::WALK);
         player.tPos = VECTOR2(player.tPos.x * XSIZE, player.tPos.y * YSIZE);
 
@@ -41,6 +48,7 @@ void Player::Update()
     } else if (STATE(0) & PAD_RIGHT) {
         player.tPos = animation.GetAnimation_Offset(Player_Animation::STATE::WALK);
         player.tPos = VECTOR2(player.tPos.x * XSIZE, player.tPos.y * YSIZE);
+
 
         player.size.x = 1;
         speed.x = 6;
@@ -82,6 +90,18 @@ void Player::Update()
     else if (!VertiChipCheck(&player, &test))
     {
         onGround = false;
+    }
+    
+    if (pushedFan) {
+        if (pushedFan->pos.x < player.pos.x && speed.x < 0 || pushedFan->pos.x > player.pos.x && speed.x > 0) {
+            pushedFan->pos.x += speed.x;
+            speed *= 0.3f;
+        }
+        else {
+            pushedFan = nullptr;
+        }
+
+        if(speed.x == 0) pushedFan = nullptr;
     }
 }
 
@@ -136,16 +156,8 @@ void Player::Wind()
 
 void Player::PushFan(Fan* fan)
 {
+    pushedFan = fan;
     ++timer;
-    if (timer > 30)
-    {
-        speed.x *= 0.3f;
-        fan->pos.x += speed.x;
-    }
-    else
-    {
-        speed = {0, 0};
-    }
     //switch (state)
     //{
     //case 1:
