b06471e0953d4dbcfb414ab2c1468d78e4e706d2
mysamrung <mysamrung@gmail.com>
2021-01-16 22:14:21 +0900

    Update UI

--- SourceCode/UI.cpp
+++ SourceCode/UI.cpp
@@ -22,9 +22,9 @@ void UI_BTN::Update() {
 		if (TRG_RELEASE(0) & PAD_START) {
 			if (_onClickEvent) _onClickEvent();
 		} 
-		color_value += (6 - color_value) * 0.01f;
+		color_value += (6 - color_value) * 0.1f;
 	} else {
-		color_value += (1 - color_value) * 0.01f;
+		color_value += (1 - color_value) * 0.1f;
 	}
 }
 
@@ -43,16 +43,28 @@ void UI_BTN::Render() {
 void UI_GP_Manage::Init() {
 	std::shared_ptr<Sprite> gear_ui		(sprite_load(L"./Data/Images/MENU ICON.png"));
 	std::shared_ptr<Sprite> title_ui	(sprite_load(L"./Data/Images/TITLE_BTN.png"));
+	std::shared_ptr<Sprite> stage_ui	(sprite_load(L"./Data/Images/STAGE_BTN.png"));
+	std::shared_ptr<Sprite> reset_ui	(sprite_load(L"./Data/Images/RESET_BTN.png"));
 
 	buttons.insert({ "PAUSE_BTN", UI_BTN({ 1700, 25 }, { 0.6f, 0.6f }, { 256, 256 }, gear_ui, [] {
 		isPause = !isPause;
 	})});
 
-	buttons.insert({ "TITLE_BTN", UI_BTN({ SCREEN_W / 2 - 300, SCREEN_H / 2}, { 0.6f, 0.6f }, { 761, 154 }, title_ui, [] {
-	isPause = !isPause;
-}) });
+	buttons.insert({ "TITLE_BTN", UI_BTN({ SCREEN_W / 2 - 300, SCREEN_H / 2 - 300}, { 0.6f, 0.6f }, { 761, 154 }, title_ui, [] {
+		
+	}) });
+
+	buttons.insert({ "STAGE_BTN", UI_BTN({ SCREEN_W / 2 - 300, SCREEN_H / 2 - 150}, { 0.6f, 0.6f }, { 761, 154 }, stage_ui, [] {
+
+	}) });
+
+	buttons.insert({ "RESET_BTN", UI_BTN({ SCREEN_W / 2 - 300, SCREEN_H / 2}, { 0.6f, 0.6f }, { 761, 154 }, reset_ui, [] {
+
+	}) });
 
 	buttons["TITLE_BTN"].isActive = false;
+	buttons["RESET_BTN"].isActive = false;
+	buttons["STAGE_BTN"].isActive = false;
 }
 
 void UI_GP_Manage::Update() {
@@ -61,6 +73,8 @@ void UI_GP_Manage::Update() {
 	}
 
 	buttons["TITLE_BTN"].isActive = isPause;
+	buttons["RESET_BTN"].isActive = isPause;
+	buttons["STAGE_BTN"].isActive = isPause;
 }
 
 void UI_GP_Manage::Render() {
